3fdf1af011c6edb7d345dc0fa979f311e2444908
Waage <Waage@129.241.209.114>
2011-10-26 13:02:48 +0200

    added central unit

--- no.ntnu.item.arctis.androidacsystem/src/no/ntnu/item/arctis/androidacsystem/centralstation/CentralStation.java
+++ no.ntnu.item.arctis.androidacsystem/src/no/ntnu/item/arctis/androidacsystem/centralstation/CentralStation.java
@@ -1,7 +1,30 @@
 package no.ntnu.item.arctis.androidacsystem.centralstation;
 
+import android.util.Log;
+import no.ntnu.item.arctis.library.objects.login.Credentials;
+import no.ntnu.item.arctis.library.proxies.Message;
 import no.ntnu.item.arctis.runtime.Block;
 
 public class CentralStation extends Block {
+
+	public Message checkMessage(Message msg) {
+		if(msg.getPayload() instanceof String){
+//			System.out.println(msg.getPayload());
+			Log.i("message", msg.getPayload().toString());
+			if(msg.getPayload().equals("erlend")){
+				Message response = new Message("un_access"); 
+				response.setReceiver(msg.getSender().getCopy());
+				response.setSender(msg.getReceiver().getCopy());	
+				response.setPayload("");
+				return response;
+			}
+			else return null;
+		}
+			else{ 
+//				System.out.println("not String: " + msg.getPayload());
+				Log.i("not string", msg.getPayload().toString());
+				return null;
+			}
+	}
 
 }
