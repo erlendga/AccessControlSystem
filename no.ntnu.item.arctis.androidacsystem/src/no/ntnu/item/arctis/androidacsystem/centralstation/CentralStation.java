54f6d48bc82f0f733d8667dfb0871f7d59ed6111
Waage <Waage@129.241.209.114>
2011-10-27 16:37:35 +0200

    laget hashTable

--- no.ntnu.item.arctis.androidacsystem/src/no/ntnu/item/arctis/androidacsystem/centralstation/CentralStation.java
+++ no.ntnu.item.arctis.androidacsystem/src/no/ntnu/item/arctis/androidacsystem/centralstation/CentralStation.java
@@ -1,16 +1,34 @@
 package no.ntnu.item.arctis.androidacsystem.centralstation;
 
+import java.util.Hashtable;
+
 import no.ntnu.item.arctis.library.objects.login.Credentials;
 import no.ntnu.item.arctis.library.proxies.Message;
 import no.ntnu.item.arctis.runtime.Block;
 
 public class CentralStation extends Block {
-
+	private Hashtable hTable = new Hashtable();
+	
+	public void setHTable(Hashtable hTable){
+		this.hTable = hTable;
+	}
+	
+	public CentralStation(){
+		Hashtable hTable = new Hashtable();
+		hTable.put("erlend", "1234");
+		hTable.put("oystein", "5555");
+		hTable.put("jonas", "6666");
+		setHTable(hTable);
+	}
+	
 	public Message checkMessage(Message msg) {
 		if(msg.getPayload() instanceof String){
-			if(msg.getPayload().equals("erlend")){
+			if(hTable.containsKey(msg.getPayload())){
 				return makeMessage(msg, "un_true");
 			}
+//			if(msg.getPayload().equals("erlend")){
+//				return makeMessage(msg, "un_true");
+//			}
 			else {
 				return makeMessage(msg, "nok");
 			}
@@ -20,9 +38,12 @@ public class CentralStation extends Block {
 			a = (Credentials)msg.getPayload();
 			String pwd = a.getPassword();
 			String usr = a.getUserName();
-			if(usr.equals("erlend") && pwd.equals("1234")){
-				return makeMessage(msg, "pin_true");	
+			if(hTable.containsKey(usr) && hTable.get(usr).equals(pwd)){
+				return makeMessage(msg, "pin_true");
 			}
+//			if(usr.equals("erlend") && pwd.equals("1234")){
+//				return makeMessage(msg, "pin_true");	
+//			}
 			else return makeMessage(msg, "nok");
 		}
 	}
